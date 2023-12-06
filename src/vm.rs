835139c9ded7544f83348a23df4680d599b573f4
梶塚太智 <kajizukataichi@outlook.jp>
2023-12-06 13:41:43 +0900

    エラーメッセージをわかりやすくした

--- src/vm.rs
+++ src/vm.rs
@@ -307,7 +307,7 @@ impl VirtualMachine {
                 21 => Instruction::WinAPI,
                 _ => {
                     self.pc += 1;
-                    self.log_print(format!("命令コード{instruction}は定義されてません"));
+                    self.log_print(format!("エラー! 命令コード{instruction}は定義されてません"));
                     continue;
                 }
             };
